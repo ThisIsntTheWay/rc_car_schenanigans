959ea6028e3cbd5fecb18c8ef9652197423c30a7
The man <ThisIsntTheWay@users.noreply.github.com>
2021-01-18 21:28:27 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,57 +1,59 @@
+/*
+  Author: V. Klopfenstein
+  
+  Disclaimer:
+    A very large portion of this code has been 'borrowed' from https://www.locarbftw.com/understanding-the-arduino-mavlink-library/
+    His article was the first to actually explain how the arduino mavlink library works.
+    Additionally, it has somewhat explained the anatomy of the MAVLink protocol.
+    
+    It was also the only public code that wasn't unnecessarily bloated. 
+    
+    For MAVLink message IDs, refer to:
+      https://mavlink.io/en/messages/common.html#messages
+
+*/
+
 #include <mavlink.h>
 #include <SoftwareSerial.h>
 
 SoftwareSerial mlSerial(12, 11); // RX / TX
 
 void setup() {
-  Serial.begin(57600); //Console output
+  Serial.begin(57600);    // Init arduino serial RX/TX
+  mlSerial.begin(57600);  // Init serial RX/TX for MAVLink interface
+  
   Serial.println("Boot complete"); //Console output
-  mlSerial.begin(57600); //RXTX from MAVLink interface
 
   request_datastream();
 }
 
 void loop() {  
   MavLink_receive();
+  
+  // TODO:
+   // Handle shit
 }
 
-//function called by arduino to read any MAVlink messages sent by serial communication from flight controller to arduino
+// Receive MAVLink stream
 void MavLink_receive() {
   mavlink_message_t msg;
   mavlink_status_t status;
 
+  // Only request if mlSerial is alive and well, i.E. not '0'
   while (mlSerial.available()) {
     uint8_t c = mlSerial.read();
 
-    //Get new message
+    // Get new message
     if (mavlink_parse_char(MAVLINK_COMM_0, c, & msg, & status)) {
 
-      //Handle new message from autopilot
+      // Handle new message from autopilot
       Serial.print("DEBUG: msgid is: ");
         Serial.println(msg.msgid); //Console output
       
       switch (msg.msgid) {
-        case MAVLINK_MSG_ID_GPS_RAW_INT: {
-          mavlink_gps_raw_int_t packet;
-          mavlink_msg_gps_raw_int_decode( & msg, & packet);
-  
-          Serial.print("\nGPS Fix: ");
-          Serial.println(packet.fix_type);
-          Serial.print("GPS Latitude: ");
-          Serial.println(packet.lat);
-          Serial.print("GPS Longitude: ");
-          Serial.println(packet.lon);
-          Serial.print("GPS Speed: ");
-          Serial.println(packet.vel);
-          Serial.print("Sats Visible: ");
-          Serial.println(packet.satellites_visible);
-  
-        }
-
-          case MAVLINK_MSG_ID_RC_CHANNELS_RAW: { // #35    
+        case MAVLINK_MSG_ID_RC_CHANNELS_RAW: { // # MSG ID: 35    
             mavlink_rc_channels_raw_t packet;
             mavlink_msg_rc_channels_raw_decode( & msg, & packet); 
-
               
             Serial.print("PX RC CHANNLES (RAW): ");
             Serial.print("[CH1: ");
@@ -73,42 +75,23 @@ void MavLink_receive() {
             Serial.println("]");
           }
         break;
-
       }
     }
   }
 }
 
+// Due to the nature of MAVLink, we must first request a datastream from the TARGET.
+// If not done prior to REQUESTING data, the MAVLink interface will be unable to handle our request.
 void request_datastream() {
-  //Request Data from Pixhawk
-  uint8_t _system_id = 1; // id of computer which is sending the command (ground control software has id of 255)
-  uint8_t _component_id = 2; // seems like it can be any # except the number of what Pixhawk sys_id is
-  uint8_t _target_system = 55; // Id # of Pixhawk (should be 1)
-  uint8_t _target_component = 0; // Target component, 0 = all (seems to work with 0 or 1
-  uint8_t _req_stream_id = MAV_DATA_STREAM_ALL;
-  uint16_t _req_message_rate = 0x01; //number of times per second to request the data in hex
-  uint8_t _start_stop = 1; //1 = start, 0 = stop
-
-  // STREAMS that can be requested
-  /*
-   * Definitions are in common.h: enum MAV_DATA_STREAM and more importantly at:
-     https://mavlink.io/en/messages/common.html#MAV_DATA_STREAM
-   *   
-   * MAV_DATA_STREAM_ALL=0, // Enable all data streams
-   * MAV_DATA_STREAM_RAW_SENSORS=1, /* Enable IMU_RAW, GPS_RAW, GPS_STATUS packets.
-   * MAV_DATA_STREAM_EXTENDED_STATUS=2, /* Enable GPS_STATUS, CONTROL_STATUS, AUX_STATUS
-   * MAV_DATA_STREAM_RC_CHANNELS=3, /* Enable RC_CHANNELS_SCALED, RC_CHANNELS_RAW, SERVO_OUTPUT_RAW
-   * MAV_DATA_STREAM_RAW_CONTROLLER=4, /* Enable ATTITUDE_CONTROLLER_OUTPUT, POSITION_CONTROLLER_OUTPUT, NAV_CONTROLLER_OUTPUT.
-   * MAV_DATA_STREAM_POSITION=6, /* Enable LOCAL_POSITION, GLOBAL_POSITION/GLOBAL_POSITION_INT messages.
-   * MAV_DATA_STREAM_EXTRA1=10, /* Dependent on the autopilot
-   * MAV_DATA_STREAM_EXTRA2=11, /* Dependent on the autopilot
-   * MAV_DATA_STREAM_EXTRA3=12, /* Dependent on the autopilot
-   * MAV_DATA_STREAM_ENUM_END=13,
-   * 
-   * Data in PixHawk available in:
-   *  - Battery, amperage and voltage (SYS_STATUS) in MAV_DATA_STREAM_EXTENDED_STATUS
-   *  - Gyro info (IMU_SCALED) in MAV_DATA_STREAM_EXTRA1
-   */
+  // Params used in the request.
+  // Primarily used to identify the client for the interface
+  uint8_t _system_id = 1; // ID of the client that is sending the command (ground control software has id of 255)
+  uint8_t _component_id = 2;
+  uint8_t _target_system = 55; // ID of target system, doesn't really matter
+  uint8_t _target_component = 0;
+  uint8_t _req_stream_id = MAV_DATA_STREAM_ALL; // Type of data stream
+  uint16_t _req_message_rate = 0x01; // Amount of requets per sec
+  uint8_t _start_stop = 1; // 1 = Start | 0 = Stop
 
   // Initialize the required buffers
   mavlink_message_t msg;
